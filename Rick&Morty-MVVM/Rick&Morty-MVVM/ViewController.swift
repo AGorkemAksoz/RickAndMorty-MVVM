4718ba852d337bb247e77dcb9059278183c1f56e
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2023-02-23 18:27:41 +0300

    Initial Commit

---
+++ Rick&Morty-MVVM/Rick&Morty-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty-MVVM
+//
+//  Created by Ali Görkem Aksöz on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
